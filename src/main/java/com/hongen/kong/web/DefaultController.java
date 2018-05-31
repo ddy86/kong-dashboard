bba876980148056ac9aa1006fc158cd7280a32af
daidongyang <henuddy@gmail.com>
2018-05-31 09:43:08 +0800

    fix direct error

--- src/main/java/com/hongen/kong/web/DefaultController.java
+++ src/main/java/com/hongen/kong/web/DefaultController.java
@@ -21,11 +21,11 @@ public class DefaultController {
 
     @GetMapping("/login")
     public String login() {
-        return "/login";
+        return "login";
     }
 
     @GetMapping("/403")
     public String error403() {
-        return "/error/403";
+        return "error/403";
     }
 }
