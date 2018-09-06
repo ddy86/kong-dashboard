a3fc19a83b44c7e0c9be6d61a9977d7122e8dc44
daidongyang <henuddy@gmail.com>
2018-09-06 18:14:13 +0800

    config kong api url by env

--- src/main/java/com/hongen/kong/service/KongDashboardServiceImpl.java
+++ src/main/java/com/hongen/kong/service/KongDashboardServiceImpl.java
@@ -29,7 +29,7 @@ import java.util.Map;
 public class KongDashboardServiceImpl implements KongDashboardService {
 
     protected static final Logger logger = LoggerFactory.getLogger(KongDashboardService.class);
-    @Value("${kong.server.url}")
+    @Value("${KONG_API_URL}")
     String kongServer;
 
     @Override
